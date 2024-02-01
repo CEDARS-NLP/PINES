65d5d45e7128f4f7323625ac0c2daaf9b5da23c0
SinghR7 <SinghR7@mskcc.org>
2024-01-31 18:31:29 -0800

    add dockerfile

--- pines.py
+++ pines.py
@@ -1,8 +1,8 @@
 import configparser
 from fastapi import FastAPI
-from pathlib import Path, PosixPath
-from fastapi.staticfiles import StaticFiles
-from transformers import AutoTokenizer, pipeline
+from contextlib import asynccontextmanager
+from pathlib import Path
+from transformers import pipeline
 import torch
 import uvicorn
 
@@ -18,7 +18,6 @@ config.read(get_parent_dir() / "config.ini")
 model_dir = config["DEFAULT"].get("ModelDir")
 model_name = config["DEFAULT"].get("ModelName")
 
-app = FastAPI()
 models_path = get_parent_dir() / model_dir / model_name
 
 # Path to your fine-tuned model and tokenizer
@@ -26,19 +25,33 @@ models_path = get_parent_dir() / model_dir / model_name
 if len(model_dir) == 0:
     raise ValueError("Please specify a model dir in config.ini")
 
-tokenizer = AutoTokenizer.from_pretrained(models_path)
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+classifier = {}
 
-classifier = pipeline('sentiment-analysis',
-                      model=models_path,
-                      device=device,
-                      truncation=True
-                     )
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Load ML Model
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    classifier["model"] = pipeline('sentiment-analysis',
+                        model=models_path,
+                        device=device,
+                        truncation=True
+                        )
+    yield
+    # Clean up
+    classifier.clear()
+
+
+app = FastAPI(title="PINES NLP Model",
+              description=f"{model_name}",
+              version="0.0.1",
+              lifespan=lifespan
+              )
+
 
 @app.post("/predict")
 async def detect(text: str):
     """Label a single text"""
-    predictions = classifier.predict(text)
+    predictions = classifier["model"].predict(text)
     return {"prediction": predictions[0],
             "model": model_name}
 
@@ -46,13 +59,13 @@ async def detect(text: str):
 @app.post("/predict_batch")
 async def detect_batch(texts: list[str]):
     """Label a batch of texts"""
-    predictions = classifier.predict(texts)
+    predictions = classifier["model"].predict(texts)
     return {"prediction": predictions,
             "model": model_name}
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app",
+    uvicorn.run("pines:app",
                 host="0.0.0.0",
                 port=8036,
                 reload=True)

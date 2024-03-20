b04460b49301cd9bc635a77ed835a0b5014f8336
ygivenx <singhrohan@outlook.com>
2024-03-20 13:17:31 -0700

    fix config

--- pines.py
+++ pines.py
@@ -21,10 +21,15 @@ def get_parent_dir():
 config = configparser.ConfigParser()
 config.read(get_parent_dir() / "config.ini")
 model_dir = config["DEFAULT"].get("ModelDir")
-model_name = config["DEFAULT"].get("ModelName")
+current_model = config["DEFAULT"].get("MODEL")
 
-models_path = get_parent_dir() / model_dir / model_name
+model_max_length = int(config[current_model].get("ModelMaxLength"))
+model_name = config[current_model].get("ModelName")
 
+if model_name.startswith("s3:"):
+    models_path = model_name
+else:
+    models_path = get_parent_dir() / model_dir / model_name
 # Path to your fine-tuned model and tokenizer
 
 if len(model_dir) == 0:
@@ -39,6 +44,7 @@ async def lifespan(app: FastAPI):
     classifier["model"] = pipeline('sentiment-analysis',
                         model=models_path,
                         device=device,
+                        max_length=model_max_length,
                         truncation=True
                         )
     yield
@@ -56,6 +62,13 @@ app = FastAPI(title="PINES NLP Model",
 @app.get("/")
 async def read_root():
     return {"message": f"Welcome to the Pines NLP Model\n\n Current Model: {model_name}"}
+
+
+@app.get("/healthcheck")
+async def healthcheck():
+    return {"message": f"Welcome to the Pines NLP Model\n\n Current Model: {model_name}",
+            "status": "Healthy"}
+
 
 @app.post("/predict")
 async def detect(note: Note) -> dict:
